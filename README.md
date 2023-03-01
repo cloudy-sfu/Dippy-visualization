60739610ea55d467d84b04dce7c1e350c3a57e98
cloudy-sfu <41314224+cloudy-sfu@users.noreply.github.com>
2023-03-01 20:23:56 +1300

    v1.0

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # Dippy Visualization
- 
+
+![](https://shields.io/badge/dependencies-Python_3.7-blue)
+
+## Usage
+
+1.   Run `pip install -r requirements.txt`.
+
+2.   Denote Python virtual environment is at `$venv`, replace `$venv/Lib/site-packages/coinor/dippy/dippy.py` with `graphviz/dippy.py`.
+
+3.   Add one argument when defining `DipProblem` class:
+     ```python
+     DipProblem(..., json_path='*.json')
+     ```
+
+     where `json_path` is the file path to save the visualization configuration.
+
+4.   Open `graphviz/index.html` in browser, and click file upload button to load the JSON file in step 3.
---
+++ graphviz/render.js
@@ -0,0 +1,66 @@
+/*
+Contains all custom javascript functionality to render graphs and allow for user interaction
+*/
+
+// let dotIndex = dots.length - 1;
+/*
+Function to render the branch and bound tree in the centrebox div container. Parses a DOT
+file from TBD format.
+*/
+function render() {
+    // Create one line dot string from multi line string array DOT
+    let dot_str = '';
+    for (let i=0; i < dots[dotIndex].length; i++) {
+        dot_str = dot_str + dots[dotIndex][i];
+    }
+    dot_str.replace("\n","");
+
+    // Render dotstring in the div just created
+    // See https://github.com/magjac/d3-graphviz/blob/master/README.md for explanation of graphviz options
+    // Graph is rendered inside an SVG inside the centerbox div
+    let graphviz = d3.select("#centerbox").graphviz();
+    graphviz.width(document.getElementById("centerbox").clientWidth)
+        .height(document.getElementById("centerbox").clientHeight)
+        .fit(true) // Graph size is scaled to fit SVG size
+        .zoom(true)// Default, enables panning and zooming of graph
+        .zoomScaleExtent([0.01, 10000]) // Sets min and max zoom level
+        .scale(0.95) // Graph size equals SVG size
+        .renderDot(dot_str) // Render the DOT string
+    // Add caption for branch/bound step
+    document.getElementById("steptitle").innerHTML = "Step " + String(getdotIndex());
+    // Center graph
+    d3.select('g').attr('transform','translate(' +
+        String(document.getElementById("centerbox").clientWidth/2) + ',' +
+        String(document.getElementById("centerbox").clientHeight/2) + ') scale(1)');
+    // Reset zoom level to base
+    graphviz.resetZoom();
+}
+
+/*
+Called on 'previous step' button click, renders the branch and bound tree for the previous step
+*/
+function prevgraph() {
+    // Update dotIndex (allowing for wraparound and negative numbers)
+    dotIndex = (dots.length + (dotIndex - 1) % dots.length) % dots.length;
+    render();
+}
+
+/*
+Called on 'reset zoom/pan' button click, re-renders the graph at default zoom and position
+*/
+function resetgraph() {
+    render();
+}
+
+/*
+Called on 'next step' button click, renders the branch and bound tree for the next step
+*/
+function nextgraph() {
+    // Update dotIndex (allowing for wraparound)
+    dotIndex = (dotIndex + 1) % dots.length;
+    render();
+}
+
+function getdotIndex() {
+    return dotIndex
+}
